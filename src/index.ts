60542cc6cfed18bed6522053af534dca4466f215
Andy Caicedo <acaicedo@pharoshouse.com>
2022-08-04 16:38:04 -0500

    Cd - cambio de puerto

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 app.use(cors())
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 app.use('/api/users', userRouter)
 
