5c8ca72574d7954d906c7a683d8aedc76893935f
Andy Caicedo <acaicedo@pharoshouse.com>
2022-08-04 19:13:17 -0500

    CD - variables de entorno en docker

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,7 +3,7 @@ import createError from 'http-errors'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET as Secret
+const accessTokenSecret = 'unipacifico' as Secret
 
 const jswt = {
 
